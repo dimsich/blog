38d12a880c2f83025e8d27590a6c7279e1cf072d
dimsich <dimsich@gmail.com>
2015-08-23 10:14:22 +0300

    modify models

--- app/Article.php
+++ app/Article.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = [
+        'category_id',
+        'name',
+        'text'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name'
+    ];
+
+    //получаем активные типы
+    public static function getList()
+    {
+        $categories = DB::table('categories')->lists('name', 'id');
+        return $categories;
+    }
 }
