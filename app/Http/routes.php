9b548604cee23c8d950e655e8795fd1b376588b5
dimsich <dimsich@gmail.com>
2015-08-23 10:12:47 +0300

    modify routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::get('/', 'HomeController@index');
+Route::get('admin', function(){return view('admin');});
+Route::get('category/{id}', 'CategoryController@show')->where('id', '[0-9]+');
+Route::get('article/{id}', 'ArticleController@show')->where('id', '[0-9]+');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::controllers([
+    'admin/categories' => 'CategoryController',
+    'admin/articles' => 'ArticleController',
+]);
